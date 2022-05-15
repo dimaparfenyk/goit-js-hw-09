28e1068d4a2b1624419c60ef610a02eb92f19f80
dimaparfenyk <92674961+dimaparfenyk@users.noreply.github.com>
2022-05-15 10:20:56 +0300

    task3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (evt) => {
+ 
+  evt.preventDefault();
+
+  let delay = Number(evt.currentTarget.delay.value);
+  const step = Number(evt.currentTarget.step.value);
+  const amount = Number(evt.currentTarget.amount.value);
+  
+  for (let position = 1; position <= amount; position += 1) {
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+    delay += step;
   }
-}
+});
+
+function createPromise(position, delay) {
+
+const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+     resolve({position, delay})
+    } 
+      reject({position, delay})
+  })
+};
+
